dd53424d0775b846754682eaa79f8747f210827b
nkechi32 <100301298+nkechi32@users.noreply.github.com>
2022-03-18 00:38:33 +0100

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include "main.h"
+
+/**
+ * mul - int mul(int a, int b)
+ * @a: first input value
+ * @b: second input value
+ *
+ * Description: multiplies two integers
+ * Return: Always (0).
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
