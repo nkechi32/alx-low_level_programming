6cb392d71b1c77aa9f20355284825fa8dd55d50d
nkechi32 <100301298+nkechi32@users.noreply.github.com>
2022-03-22 00:29:25 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,17 @@
-#!/bin/bash
+#include "main.h"
+
+/**
+ * _puts - prints string
+ * @str: input string
+ * Return: no return
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
